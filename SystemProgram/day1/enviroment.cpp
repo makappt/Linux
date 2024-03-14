7cb4fd5d0b0bf0e33e335af4025c6daf15c3d41e
makappt <makappt@qq.com>
2024-03-14 19:57:28 +0800

    enviroment

--- SystemProgram/day1/enviroment.cpp
+++ SystemProgram/day1/enviroment.cpp
@@ -1,14 +1,67 @@
-// 添加或删除环境变量
+// 增删查环境变量
 #include <iostream>
+#include <stdlib.h>
+
 using namespace std;
+
 extern char **environ;
 
 int main()
 {
     // 打印环境变量
-    for (int i = 0; environ[i] != nullptr; i++)
+    // for (int i = 0; environ[i] != nullptr; i++)
+    // {
+    //     cout << environ[i] << endl;
+    // }
+    // 查询环境变量
+    char *envValue = getenv("ABCD");
+    if (envValue == nullptr)
     {
-        cout << environ[i] << endl;
+        cout << "the env is not existed! \n";
+    }
+    else
+    {
+        cout << "ABCD = " << envValue << '\n';
+    }
+    // 添加环境变量
+    int ret = setenv("ABCD", "jianzhe", 1);
+    if (ret == 0)
+    {
+        cout << "setenv is successful!\n";
+    }
+    else
+    {
+        cout << "setenv error\n";
+    }
+
+    envValue = getenv("ABCD");
+    if (envValue == nullptr)
+    {
+        cout << "the env is not existed! \n";
+    }
+    else
+    {
+        cout << "ABCD = " << envValue << '\n';
+    }
+
+    ret = unsetenv("ABCD");
+    if (ret == 0)
+    {
+        cout << "unsetenv is successful\n";
+    }
+    else
+    {
+        cout << "unsetenv is failed!\n";
+    }
+
+    envValue = getenv("ABCD");
+    if (envValue == nullptr)
+    {
+        cout << "the env is not existed! \n";
+    }
+    else
+    {
+        cout << "ABCD = " << envValue << '\n';
     }
 
     return 0;
