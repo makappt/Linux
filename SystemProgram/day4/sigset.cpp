62d5a8e45c61d45fd7df591b7554a8adf155fb57
makappt <makappt@qq.com>
2024-03-21 20:21:14 +0800

    sigset

---
+++ SystemProgram/day4/sigset.cpp
@@ -0,0 +1,44 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void printset(sigset_t *ped)
+{
+    int i;
+    for (i = 1; i < 32; i++)
+    {
+        if ((sigismember(ped, i) == 1))
+        {
+            putchar('1');
+        }
+        else
+        {
+            putchar('0');
+        }
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    sigset_t set, ped;
+#if 0
+	sigemptyset(&set);
+	sigaddset(&set, SIGINT);
+	sigaddset(&set, SIGQUIT);
+	sigaddset(&set, SIGKILL);
+	sigaddset(&set, SIGSEGV);
+#endif
+    sigfillset(&set);
+    sigprocmask(SIG_BLOCK, &set, NULL); // 不获取原屏蔽字
+    // 注意！！！，即使信号加入信号屏蔽字，未决信号集也不会改变，只有信号产生才会改变！！！
+    while (1)
+    {
+        sigpending(&ped); // 获取未决信号集
+        printset(&ped);
+        sleep(1);
+    }
+
+    return 0;
+}
