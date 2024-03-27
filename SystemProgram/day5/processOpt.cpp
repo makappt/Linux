0155cbc2186602b278aa9b3083aea63efa2084bd
makappt <makappt@qq.com>
2024-03-27 19:17:59 +0800

    进程组相关函数

---
+++ SystemProgram/day5/processOpt.cpp
@@ -0,0 +1,40 @@
+/*
+进程组相关函数
+getpgrp：获得当前进程进程组id
+getpgid: 获得指定进程进程组id
+setpgid：改变指定进程的所属进程组
+*/
+
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        perror("fork error");
+        exit(-1);
+    }
+    if (pid == 0)
+    {
+        std::cout << "子进程id = " << getpid() << std::endl;
+        std::cout << "子进程的组id = " << getpgrp() << std::endl;
+        std::cout << "子进程的组id = " << getpgid(0) << std::endl;
+        sleep(5);
+        std::cout << "---------------" << std::endl;
+        std::cout << "子进程id = " << getpid() << std::endl;
+        std::cout << "子进程的组id = " << getpgrp() << std::endl;
+        std::cout << "子进程的组id = " << getpgid(0) << std::endl;
+    }
+    if (pid > 0)
+    {
+        setpgid(pid, pid);
+        while (waitpid(-1, nullptr, WNOHANG))
+            ;
+        while (1)
+            ;
+    }
+    return 0;
+}
